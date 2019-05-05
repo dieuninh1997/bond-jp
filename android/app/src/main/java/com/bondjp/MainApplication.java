59cc13efd800b5c17cf00f2f6c9de4d3a0b19c6c
dieuninh1997 <ttdn1997@gmail.com>
2019-05-05 19:44:58 +0700

    init

--- android/app/src/main/java/com/bondjp/MainApplication.java
+++ android/app/src/main/java/com/bondjp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.todoredux;
+package com.bondjp;
 
 import android.app.Application;
 
--- ios/bondjpTests/bondjpTests.m
+++ ios/bondjpTests/bondjpTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface TodoReduxTests : XCTestCase
+@interface bondjpTests : XCTestCase
 
 @end
 
-@implementation TodoReduxTests
+@implementation bondjpTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
